b27354595eaf9874bd1d27000d428ad968f13cfd
SaadSKS <saad.sks@gmail.com>
2019-11-12 21:21:31 +0530

    Plane Detection Setup
    Basic plane detection scene setup and built for android

---
+++ AR Samples/Assets/Scripts/ARCenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ARCenter : MonoBehaviour
+{
+    public TextMeshProUGUI TrackableBTN;
+
+
+    void Start()
+    {
+        
+    }
+    
+
+    void Update()
+    {
+        
+    }
+}
