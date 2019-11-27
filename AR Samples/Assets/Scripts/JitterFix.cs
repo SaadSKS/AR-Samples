573d81938ad47dc68ba85b0ec0d2e6c0a84e5b13
SaadSKS <saad.sks@gmail.com>
2019-11-27 16:19:15 +0530

    Shader Pottery Alpha
    Alpha stage of pottery sub-project is ready. Customizable with 2 pattern support. Considering upgrading to 3-4 pattern support. Texturing and PBR materials will be added next

---
+++ AR Samples/Assets/Scripts/JitterFix.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JitterFix : MonoBehaviour
+{
+    public bool simulateJitter = false;
+    public bool jitterSmoothFix = false;
+    public float smoothTime = 1f;
+    public float jitterMagnitude = 0.2f;
+
+    private Vector3 velocity = Vector3.zero;
+    Vector2 targetPos;
+    Transform tf;
+
+    void Start()
+    {
+        simulateJitter = false;
+        jitterSmoothFix = false;
+        targetPos = Vector2.zero;
+        tf = GetComponent<Transform>();
+    }
+
+    
+    void Update()
+    {
+        if (simulateJitter)
+        {
+            JitterShuffle();
+            if (jitterSmoothFix)
+            {
+                tf.position = Vector3.SmoothDamp(tf.position, new Vector3(targetPos.x, 0, targetPos.y), ref velocity, smoothTime);
+            }
+            else
+            {
+                tf.position = new Vector3(targetPos.x, 0, targetPos.y);
+            }
+            
+        }
+        else
+        {
+            targetPos = Vector2.zero;
+            tf.position = new Vector3(targetPos.x, 0, targetPos.y);
+        }
+
+
+    }
+
+    void JitterShuffle()
+    {
+        targetPos = Random.onUnitSphere * jitterMagnitude;
+    }
+
+
+}
